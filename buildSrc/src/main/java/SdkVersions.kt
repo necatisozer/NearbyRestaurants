c604150f78d25c5326ec34f79bf93576634da196
necati <necatisozer@gmail.com>
2020-07-21 05:17:16 +0300

    Bump NDK version to 21.3.6528147

--- buildSrc/src/main/java/SdkVersions.kt
+++ buildSrc/src/main/java/SdkVersions.kt
@@ -2,5 +2,5 @@ object SdkVersions {
     const val compileSdkVersion = 29
     const val minSdkVersion = 21
     const val targetSdkVersion = 29
-    const val ndkVersion = "21.1.6352462"
+    const val ndkVersion = "21.3.6528147"
 }
